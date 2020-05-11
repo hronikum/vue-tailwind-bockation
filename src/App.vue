74295e204715827e7a5370ed2c10c99462d9429d
Zhostya Kilin <kostya.zhilin@gmail.com>
2020-05-11 15:57:08 +0300

    Add tailwind to project.Start from scratch

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1 class="text-6xl font-bold text-purple-500">Hello from Tailwind</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+  components: {}
+}
+
+</script>
+<style>
+</style>
